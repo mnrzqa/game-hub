a3b80376611101d705505bf3780eb88cfbe48903
Naja <trirazaqa@gmail.com>
2025-06-27 22:44:03 +0700

    Refactor platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import platfroms from "../data/platfroms";
+import axios from "axios";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 interface Platform {
   id: number;
@@ -6,6 +10,15 @@ interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platfroms, error: null });
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24h
+    initialData: { count: platfroms.length, results: platfroms },
+  });
 
 export default usePlatforms;
