9911776c33de689cfaf8fdef3a941eba4da0f015
Naja <trirazaqa@gmail.com>
2025-06-27 23:00:07 +0700

    Refacotring - fetch games with React Query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import platfroms from "../data/platfroms";
 import axios from "axios";
 import apiClient from "../services/api-client";
-import { FetchResponse } from "./useData";
+import { FetchResponse } from "../services/api-client";
 
 interface Platform {
   id: number;
