e71a3c0333a9068b58841ac3864a6ddd89131561
Naja <trirazaqa@gmail.com>
2025-06-28 10:19:55 +0700

    Refactoring- Remove duplicate interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
