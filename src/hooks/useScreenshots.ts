b5ef567c6825aaee992fb8fc272f1ce210bee9af
Naja <trirazaqa@gmail.com>
2025-06-29 20:09:21 +0700

    Refactor entity interfaces

--- src/hooks/useScreenshots.ts
+++ src/hooks/useScreenshots.ts
@@ -1,6 +1,6 @@
-import APIClient from "../services/api-client";
 import { useQuery } from "@tanstack/react-query";
-import { Screenshot } from "../entities/Screenshot";
+import Screenshot from "../entities/Screenshot";
+import APIClient from "../services/api-client";
 
 const useScreenshots = (gameId: number) => {
   const apiClient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
